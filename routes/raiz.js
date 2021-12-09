bb352ca388e25f16a13a39ca87cc65a22af3d2ac
Guido Rafael <guidorafael23@gmail.com>
2021-12-09 19:47:58 -0300

    aperfeiçoamento da view e da css proposta

--- routes/raiz.js
+++ routes/raiz.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   /* res.send('listar propostas'); */ 
+  let id = 1;
   res.render('raiz', { title: 'FreelancerJA' });
 });
 
