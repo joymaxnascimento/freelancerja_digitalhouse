f98b6a3afae6a4ee2c3366f2608fc7c63ef8a32e
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-01-21 20:01:42 -0300

    Criação do Model - Proposta.

---
+++ models/Proposta.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataType) => {
+    const Proposta = sequelize.define('Proposta', {
+        idproposta:{
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        valor_proposto_freelancer:{
+            type: DataType.FLOAT(10, 2),
+            allowNull: false
+        },
+        aceite_cliente:{
+            type: DataType.BOOLEAN,
+            allowNull: false
+        },
+        data_entrega:{
+            type: DataType.DATE,
+            allowNull: false
+        }
+    },{
+        tableName: 'servico',
+        timestamps: false
+    })
+    Proposta.associate = (model) => {
+        Proposta.hasOne(model.Servico, {
+          foreignKey: 'id_servico',
+          as: 'servico'
+        })
+      }
+
+    Proposta.associate = (model) => {
+        Proposta.hasOne(model.Usuario, {
+          foreignKey: 'idusuario_freelancer',
+          as: 'usuario'
+        })
+      }
+
+    return Proposta
+}
