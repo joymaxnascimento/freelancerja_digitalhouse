919992ed006ba19a0ddf23ca085e11565e3c697f
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-01-21 20:01:11 -0300

    Criação do model - Usuario

---
+++ models/Usuario.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, DataType) => {
+    const Usuario = sequelize.define('Usuario', {
+        idusuario:{
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome:{
+            type: DataType.STRING,
+            allowNull: false
+        },
+        senha:{
+            type: DataType.STRING,
+            allowNull: false
+        },
+        data_cadastro:{
+            type: DataType.DATE,
+            allowNull: false
+        },
+        data_atualizacao:{
+            type: DataType.DATE,
+            allowNull: false
+        },
+        email:{
+            type: DataType.STRING,
+            allowNull: false
+        },
+        status:{
+            type: DataType.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        },
+    },{
+        tableName: 'usuario',
+        timestamps: false
+    })
+
+    Usuario.associate = (model) => {
+        Usuario.hasMany(model.Proposta, {
+            foreignKey: 'idusuario_freelancer',
+            as: 'proposta'
+        })
+    }
+
+    Usuario.associate = (model) => {
+        Usuario.hasMany(model.Servico, {
+            foreignKey: 'idusuario_cliente',
+            as: 'servico'
+        })
+    }
+
+
+
+    return Usuario
+}
