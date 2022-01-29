b6d248cb6c6a7d78765ffe9f01a7393dec188b3c
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-01-28 23:58:35 -0300

    Criação do controller - Cadastro de proposta.

--- controllers/CadPropController.js
+++ controllers/CadPropController.js
@@ -1,25 +1,37 @@
-//  const { Cliente, Sequelize } = require('../models');
+const { TipoServico, Servico, Proposta } = require('../database/models')
 
-module.exports = {
-    async index(req, res, next){ 
-        
-        res.render('proposta', { title: 'Proposta' });
-  
+let CadPropController = {
+    viewForm: async (req, res) =>{
+
+        let tiposServicos = await TipoServico.findAll({order: ['servico']})
+
+        let servicos = await Servico.findAll({order: ['descricao'], include: {
+            model: Proposta,
+            required: false
+        }})
+
+        return res.render('cadastro_proposta_freelancer', {title: 'Proposta', tiposServicos: tiposServicos, servicos: servicos})
     },
-  
-    async cadastrar(req, res, next){
-    },
-  
-    async salvar(req, res, next){
-    },
-  
-    async editar(req, res, next){
-    },
-  
-    async atualizar(req, res, next){freelancer
-  
-    },
-  
-    async excluir(req, res, next){
-    },
-  }
+    salvarForm: async (req, res) => {
+
+        `idproposta`,`valor_proposto_freelancer`,`aceite_cliente`,`idusuario_freelancer`
+
+        const {idservico, idusuario_freelancer, valor_proposto_freelancer} = req.body
+
+        console.log('\n\n req_body proposta_controller \n\n')
+
+        console.log(req.body)
+
+        const salvar = await Proposta.create({
+            idservico, 
+            idusuario_freelancer, 
+            valor_proposto_freelancer,
+            aceite_cliente: 0
+        })
+
+        res.render('propostacriada', {title: 'Proposta Criada'})
+        console.log('salvar form proposta\n\n' + salvar)
+    }
+}
+
+module.exports = CadPropController
