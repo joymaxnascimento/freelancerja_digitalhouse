cb8f6dc8453ec7ff884e88f299eac69ce452b9b6
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-01-28 23:59:20 -0300

    Exclusão de arquivos de teste.

--- routes/cliente.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const ClienteController = require("../controllers/ClienteController");
-
-/* GET home page. */
-router.get('/', ClienteController.index );
-
-
-module.exports = router;
--- routes/propParaVoce.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const PropParaVoceController = require("../controllers/PropParaVoceController");
-
-/* GET home page. */
-router.get('/', PropParaVoceController.index );
-
-
-module.exports = router;
