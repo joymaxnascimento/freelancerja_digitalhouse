f2c0e9c3761330111265b8a5f2d1167ceb22fb30
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-02-03 22:25:05 -0300

    Ajuste na criptografia da senha.

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,53 +1,49 @@
 const { Usuario } = require('../database/models')
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
 const bcrypt = require('bcryptjs')
 
+const UsuarioController = {
+    viewForm: async (req, res) => {
 
-const  UsuarioController = {
-     viewForm: async (req, res) =>{
-        
-        let usuarios = await Usuario.findAll({order: ['nome']})
+        let usuarios = await Usuario.findAll({ order: ['nome'] })
 
-        return res.render('cadastro_usuario', {title: 'Cadastro de Usuário', usuarios: usuarios})
+        return res.render('cadastro_usuario', { title: 'Cadastro de Usuário', usuarios: usuarios })
     },
     salvarForm: async (req, res) => {
-        const {nome, senha, email} = req.body
+        const { nome, senha, email } = req.body
 
         console.log(req.body)
 
         const salvar = await Usuario.create({
             nome,
-            senha/* : bcrypt.hashSync(senha, 10) */, 
-            data_cadastro: new Date(), 
-            data_atualizacao: new Date(), 
+            senha: bcrypt.hashSync(senha, 10),
+            data_cadastro: new Date(),
+            data_atualizacao: new Date(),
             email,
-            status:1
+            status: 1
         })
 
-        res.render('cadastrocriado', {title: 'Cadastro Criado'})
+        res.render('cadastrocriado', { title: 'Cadastro Criado' })
         console.log('salvar form servico\n\n' + salvar)
     },
-    loginForm: (req, res) =>{
-        res.render('login', {title: 'Login'})
+
+    loginForm: (req, res) => {
+        res.render('login', { title: 'Login' })
     },
-    logarUsuario: async (req, res) =>{
-        let {email, senha} = req.body
 
-        console.log(req.body)
+    logarUsuario: async (req, res) => {
+        let { email, senha } = req.body
 
-        let usuario = await Usuario.findOne({where: {email: email}})
+        let usuario = await Usuario.findOne({ where: { email: email } })
 
-                
-        if(senha != usuario.senha){
+        console.log('\n\nreq.session.usuario.idusuario\n\n' + usuario + '\n\n')
+
+        if (!bcrypt.compareSync(senha, usuario.senha)) {
             return res.send('Senha inválida!')
         }
 
         req.session.usuario = usuario
 
-        console.log('\n\nreq.session.usuario.idusuario\n\n' + req.session.usuario.idusuario + '\n\n' )
-
-        res.render('inicio', {title: 'Início'})
+        res.render('inicio', { title: 'Início' })
     }
 }
 
