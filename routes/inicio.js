84d6c4a6bc5f25815b69f0a551cc3fa8b847305c
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-02-08 12:57:38 -0300

    Inclusão do nome do usuário logado, via session.

--- routes/inicio.js
+++ routes/inicio.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('inicio', { title: 'FreelancerJA' });
+  res.render('inicio', { title: 'Freelancer JÁ', loginCadastroUsuario: req.session.usuario.nome, linkLogin: '/' });
 });
 
 
