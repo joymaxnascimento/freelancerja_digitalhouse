4ccca33dc2471ab6d8140a570998a548bb73d958
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-02-08 12:59:14 -0300

    Criação da rota de logout.

---
+++ routes/logoutRouter.js
@@ -0,0 +1,8 @@
+let express = require('express')
+let router = express.Router()
+
+let CadUsuarioController = require("../controllers/UsuarioController")
+
+router.get('/', CadUsuarioController.logoutUsuario)
+
+module.exports = router
