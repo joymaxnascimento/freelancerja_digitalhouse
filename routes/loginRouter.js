2ac100c1a2ce6997108b882738198f6a5528666f
pauloadm2008 <pauloadm2008@gmail.com>
2022-02-14 21:43:52 -0300

    Criação da tela Fale Conosco e ajuste de rota desta tela

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -5,4 +5,8 @@ var UsuarioController = require('../controllers/UsuarioController')
 router.post('/submit', UsuarioController.logarUsuario)
 router.get('/', UsuarioController.loginForm)
 
+router.get("/faleconosco", function(req, res) {
+    res.render("fale_conosco")
+  });
+
 module.exports = router;
