986a1a9aeaa06f3ac4f40fb2b871c5d2b9b8b698
Guido Rafael <guidorafael23@gmail.com>
2022-02-22 15:35:10 -0300

    por sugestao Fredson - email unique 'email' - motivo - estava dando problema

--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataType) => {
         email: {
             type: DataType.STRING,
             allowNull: false,
-            unique: true
+            unique: 'email'
         },
         status: {
             type: DataType.BOOLEAN,
