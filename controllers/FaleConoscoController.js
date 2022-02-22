ab54a391dbe6b5864f7071ef1861a2990a0be74f
Guido Rafael <guidorafael23@gmail.com>
2022-02-21 21:10:35 -0300

    Fazendo "fale conosco" funcionar junto de Fredson !

---
+++ controllers/FaleConoscoController.js
@@ -0,0 +1,7 @@
+const  FaleConoscoController = {
+    viewForm:  (req, res) =>{
+        return res.render('fale_conosco', {title: 'Fale Conosco'})
+    }
+    }
+
+module.exports = FaleConoscoController
