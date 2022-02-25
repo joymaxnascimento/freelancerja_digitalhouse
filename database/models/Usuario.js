78886830037b2ac527fc6ba9bb5a3d40a28265c9
Guido Rafael <guidorafael23@gmail.com>
2022-02-25 17:50:23 -0300

    Acerto model usuario (truers!!!!)

--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataType) => {
         email: {
             type: DataType.STRING,
             allowNull: false,
-            unique: false
+            unique: true
         },
         status: {
             type: DataType.BOOLEAN,
