53ffc6124521a0d3b6f15be10bd2fdc2002001dd
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-02-27 12:42:44 -0300

    Retirada da rota faleconosco do router de login.

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var UsuarioController = require('../controllers/UsuarioController')
+let express = require('express');
+let router = express.Router();
+let UsuarioController = require('../controllers/UsuarioController')
 
 router.post('/submit', UsuarioController.logarUsuario)
 router.get('/', UsuarioController.loginForm)
-
-router.get("/faleconosco", function(req, res) {
-    res.render("fale_conosco")
-  });
 
 module.exports = router;
