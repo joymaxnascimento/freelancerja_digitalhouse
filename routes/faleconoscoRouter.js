10ae8a5752a516df4f06b34dec0d278acfa6f244
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-02-27 12:44:21 -0300

    limpeza no arquivo e alteração das variáveis let

--- routes/faleconoscoRouter.js
+++ routes/faleconoscoRouter.js
@@ -1,11 +1,7 @@
-var express = require('express');
-var router  = express.Router();
+let express = require('express');
+let router  = express.Router();
 
 let FaleConoscoController = require("../controllers/FaleConoscoController")
-/* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('fale_conosco', { title: 'Fale Conosco' });
-//});
 
 router.get('/', FaleConoscoController.viewForm)
 
