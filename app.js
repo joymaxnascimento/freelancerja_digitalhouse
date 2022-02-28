3136bfaa1dd7e72fb5d74205c28da6588efad2cd
joymaxnascimento <joymaxnascimento@yahoo.com.br>
2022-02-28 13:56:01 -0300

    Inclusão do link home na página de erros

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: 'Página não encontrada!', loginCadastroUsuario: '', linkLogin: ''});
+  res.render('error', {title: 'Página não encontrada!', linkHome: '/', loginCadastroUsuario: '', linkLogin: ''});
 });
 
 module.exports = app;
