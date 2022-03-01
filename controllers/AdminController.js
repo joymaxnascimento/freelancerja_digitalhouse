75330c365ff2d673b8d609ba824e3477f8d4ad98
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-02-28 23:58:09 -0300

    Tratamento - erro: Cadastro dupl. Tipo de serviço.

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,21 +1,33 @@
-const {TipoServico} = require("../database/models")
-const  AdminController = {
-    admin:  (req, res) =>{
-        return res.render('admin', {title: 'Administracao', linkHome:'/', linkLogin: '/', loginCadastroUsuario: 'login' })
+const { nextTick } = require("process")
+const { TipoServico } = require("../database/models")
+const AdminController = {
+
+    admin: (req, res) => {
+        return res.render('admin', { title: 'Administracao', linkHome: '/', linkLogin: '/', loginCadastroUsuario: 'login' })
     },
-    tiposervicoView:(req, res) =>{
-        return res.render('cadastro_tipo_servico', {title: 'Cadastro Tipo de Servico', linkHome:'/', linkLogin: '/', loginCadastroUsuario: 'login' })
-    }, 
-    tiposervicoSalvar: async (req, res) =>{
-        const {tiposervico} = req.body;
-        const salvar = await TipoServico.create(
-            {
-                servico: tiposervico
-            }
-        )
-        res.send('Cadastrado')
-        console.log(salvar)        
-    } 
+
+    tiposervicoView: (req, res) => {
+        return res.render('cadastro_tipo_servico', { title: 'Cadastro Tipo de Servico', linkHome: '/', linkLogin: '/', loginCadastroUsuario: 'login' })
+    },
+
+    tiposervicoSalvar: async (req, res) => {
+        const { tiposervico } = req.body
+
+        let servicoExistente = await TipoServico.findOne({ where: { servico: tiposervico } })
+
+        if (!servicoExistente) {
+            await TipoServico.create(
+                {
+                    servico: tiposervico
+                }
+            )
+
+            res.send('Cadastrado')
+            
+        } else {
+            res.send('Serviço já existe no banco!')
+        }
     }
+}
 
 module.exports = AdminController
