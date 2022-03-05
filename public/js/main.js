7362a349feb5028207ba71d719e36004684d490b
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-03-05 12:32:58 -0300

    Implementada  busca de texto na lista de serviços.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,20 @@
 window.addEventListener('load', () => {
+    /*
+    View: cadastro_usuario.ejs
+    */
     let frmCadUsuario = document.getElementById('formCadastroUsuario')
 
-    if(frmCadUsuario != null){
+    if (frmCadUsuario != null) {
         frmCadUsuario.addEventListener('submit', (e) => {
             let ulErros = document.querySelector('div.erros ul')
             let erros = []
             let senha = document.getElementById('senha')
             let repetirSenha = document.getElementById('repetirSenha')
-    
+
             if (senha.value !== repetirSenha.value) {
                 erros.push('As senhas não coincidem!')
             }
-    
+
             if (erros.length > 0) {
                 e.preventDefault()
                 erros.forEach((erro) => {
@@ -21,4 +24,29 @@ window.addEventListener('load', () => {
             }
         })
     }
+
+    /*
+    Lista de serviços
+    View: cadastro_proposta_freelancer.ejs
+    */
+
+    let textoBusca = document.querySelector('.buscar')
+
+    if (textoBusca != null) {
+        let listaServicos = document.querySelector('.cards')
+        let servicos = listaServicos.getElementsByClassName('card')
+
+        textoBusca.addEventListener('keyup', (e) => {
+            for (let i = 0; i < servicos.length; i++) {
+                let texto = servicos[i].querySelector('p');
+
+                if (texto.textContent.toLocaleUpperCase().indexOf(e.target.value.toUpperCase()) > -1) {
+                    servicos[i].style.display = ''
+                }else{
+                    servicos[i].style.display = 'none'
+                }
+            }
+        })
+    }
+
 })
