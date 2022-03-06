08ab8160f2ff8b560cfb3eb45d497d2f639e0183
Guido Rafael <guidorafael23@gmail.com>
2022-03-06 15:15:27 -0300

    mera identacao de inhas com mais de 90 colunas (findAll de servicos e duas renderizacoes)

--- controllers/CadPropController.js
+++ controllers/CadPropController.js
@@ -8,13 +8,20 @@ let CadPropController = {
         let tiposServicos = await TipoServico.findAll({ order: ['servico'] })
 
         let servicos = await Servico.findAll({
-            where: { [Op.not]: { idusuario_cliente: req.session.usuario.idusuario } }, order: ['descricao'], include: {
-                model: Proposta,
-                required: false
-            }
+            where: { [Op.not]: { idusuario_cliente: req.session.usuario.idusuario } }, 
+            order: ['data_entrega', 'descricao'], 
+            include: [ {model: Proposta, required: false}, 
+                     ]
         })
 
-        return res.render('cadastro_proposta_freelancer', { title: 'Proposta', linkHome: '/inicio', tiposServicos: tiposServicos, servicos: servicos, loginCadastroUsuario: req.session.usuario.nome, linkLogin: '/', formulario:'' })
+        return res.render('cadastro_proposta_freelancer', 
+                           { title: 'Proposta', 
+                             linkHome: '/inicio', 
+                             tiposServicos: tiposServicos, 
+                             servicos: servicos, 
+                             loginCadastroUsuario: req.session.usuario.nome, 
+                             linkLogin: '/', 
+                             formulario:'' })
     },
     salvarForm: async (req, res) => {
 
@@ -33,9 +40,36 @@ let CadPropController = {
             aceite_cliente: 0
         })
 
-        res.render('propostacriada', { title: 'Proposta Criada', linkHome: '/inicio', loginCadastroUsuario: req.session.usuario.nome, linkLogin: '/' })
+        res.render('propostacriada', 
+                   { title: 'Proposta Criada', 
+                     linkHome: '/inicio', 
+                     loginCadastroUsuario: req.session.usuario.nome, 
+                     linkLogin: '/' })
         console.log('salvar form proposta\n\n' + salvar)
     }
 }
 
 module.exports = CadPropController
+
+
+
+/*  exemplo copiado do manual do sequelize para mera compreensao da sintaxe
+https://sequelize.org/master/manual/eager-loading.html
+
+The include option can receive an array in order to fetch multiple associated models at once:
+
+Foo.findAll({
+  include: [
+    {
+      model: Bar,
+      required: true
+    },
+    {
+      model: Baz,
+      where: // bla bla bla 
+    },
+    Qux // Shorthand syntax for { model: Qux } also works here
+  ]
+})    
+
+*/
