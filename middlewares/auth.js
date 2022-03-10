caf3226f82d57f7f2bf997de03886b9339e4924c
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-03-09 23:23:00 -0300

    Ajuste no middleware de autenticação.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 function auth(req, res, next) {
-    if (typeof (req.session.usuario) != 'undefined') {
+    if (req.session.usuario) {
         return next()
     } else {
         return res.redirect('/')
