7f192fbdb7d94bfedb02ce542832a5157987ca47
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-03-10 22:26:55 -0300

    Formulário de fale conosco.

--- controllers/FaleConoscoController.js
+++ controllers/FaleConoscoController.js
@@ -1,6 +1,10 @@
 const FaleConoscoController = {
     viewForm: (req, res) => {
-        return res.render('fale_conosco', { title: 'Fale Conosco' })
+        return res.render('fale_conosco', { title: 'Fale Conosco', linkHome:'/', linkLogin: '/', loginCadastroUsuario: 'Cadastro'})
+    },
+    envioForm: (req, res) => {
+        res.locals.mensagemEnviada = true
+        return res.render('fale_conosco', { title: 'Fale Conosco', linkHome:'/', linkLogin: '/', loginCadastroUsuario: 'Cadastro'})
     }
 }
 
--- routes/faleconoscoRouter.js
+++ routes/faleconoscoRouter.js
@@ -4,5 +4,6 @@ let router  = express.Router();
 let FaleConoscoController = require("../controllers/FaleConoscoController")
 
 router.get('/', FaleConoscoController.viewForm)
+router.post('/enviar', FaleConoscoController.envioForm)
 
 module.exports = router;
