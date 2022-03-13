4fb6340aaa77753addeac60a3d43d7cb732e7577
Guido Rafael <guidorafael23@gmail.com>
2022-03-13 17:21:31 -0300

    continua a implementacao da listagem de OS SERVICOS com finalidade de facilitar a administracao do sistema. Falta ativar botoes formatar data.

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,4 +1,4 @@
-const { TipoServico } = require("../database/models")
+const { TipoServico , Servico } = require("../database/models")
 const AdminController = {
 
     admin: (req, res) => {
@@ -11,33 +11,31 @@ const AdminController = {
 
     tiposervicoView: (req, res) => {
         return res.render('cadastro_tipo_servico', 
-                           { title: 'Cadastro Tipo de Servico', 
-                             linkHome: '/', 
-                             linkLogin: '/', 
-                             loginCadastroUsuario: 'login' })
+        { title: 'Cadastro Tipo de Servico', 
+          linkHome: '/', 
+          linkLogin: '/', 
+          loginCadastroUsuario: 'login' })
     },
 
     listartiposervicoView: async (req, res) => {
         let tpservicos = await TipoServico.findAll({order: ['servico']});
         return res.render('adm_listar_tipo_servico', 
-          { title: 'Listar Tipos de Servico', 
-            linkHome:   '/', 
-            tpservicos: tpservicos,
-            linkLogin:  '/', 
-            loginCadastroUsuario: 'login' })
+        { title: 'Listar Tipos de Servico', 
+          linkHome:   '/', 
+          tpservicos: tpservicos,
+          linkLogin:  '/', 
+          loginCadastroUsuario: 'login' })
     },
 
     listarservicoView: async (req, res) => {
-        let tpservicos = await TipoServico.findAll({order: ['servico']});
-        return res.render('adm_listar_servico', 
-          { title: 'Listar Tipos de Servico', 
+        let osservicos = await Servico.findAll({order: ['idservico']});
+        return res.render('adm_listar_osservicos', 
+          { title: 'Listar os Servicos', 
             linkHome:   '/', 
-            tpservicos: tpservicos,
             linkLogin:  '/', 
-            loginCadastroUsuario: 'login' })
+            loginCadastroUsuario: 'login',
+            osservicos: osservicos })
     },
-
-
 
 
     tiposervicoSalvar: async (req, res) => {
@@ -60,5 +58,38 @@ const AdminController = {
         }
     }
 }
+
+// module.exports = AdminController
+
+
+
+/**
+ * Number.prototype.format(n, x, s, c)
+ * 
+ * @param integer n: length of decimal
+ * @param integer x: length of whole part
+ * @param mixed   s: sections delimiter
+ * @param mixed   c: decimal delimiter
+ */
+//  Number.prototype.format = function(n, x, s, c) {
+//     var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+//         num = this.toFixed(Math.max(0, ~~n));
+
+//     return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+// };
+// function ParseFloat(str,val) {
+//     str = str.toString();
+//     str = str.slice(0, (str.indexOf(".")) + val + 1); 
+//     return Number(str);   
+// }
+
+function format2(n, currency) {
+    console.log(n);
+    var nn = n.toFixed(2)
+    console.log(nn)
+    return currency + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+}
+
+// 
 
 module.exports = AdminController
