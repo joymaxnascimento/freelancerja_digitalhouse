6d82dc2c46eb01416e05b49547dbd736ea8e8a51
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-03-13 23:39:23 -0300

    Remoção da captura de erros do cadastro de usuário

--- public/js/main.js
+++ public/js/main.js
@@ -8,34 +8,7 @@ window.addEventListener('load', () => {
 
     document.addEventListener('click', (e) =>{
         msgServicoCriado.style.display = 'none'
-    })
-
-
-    /*
-    View: cadastro_usuario.ejs
-    */
-    var frmCadUsuario = document.getElementById('formCadastroUsuario')
-
-    if (frmCadUsuario != null) {
-        frmCadUsuario.addEventListener('submit', (e) => {
-            let ulErros = document.querySelector('div.erros ul')
-            let erros = []
-            let senha = document.getElementById('senha')
-            let repetirSenha = document.getElementById('repetirSenha')
-
-            if (senha.value !== repetirSenha.value) {
-                erros.push('As senhas não coincidem!')
-            }
-
-            if (erros.length > 0) {
-                e.preventDefault()
-                erros.forEach((erro) => {
-                    console.log(erro)
-                    ulErros.innerHTML += '<li>' + erro + '<li>'
-                })
-            }
-        })
-    }
+    })    
 
     /*
     Lista de serviços
