67942f08cbb796b8ab66e7ef6061280db784dc73
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2022-04-18 17:59:36 -0300

    Salvando no banco, as mensagens do fale conosco.

--- controllers/FaleConoscoController.js
+++ controllers/FaleConoscoController.js
@@ -1,8 +1,19 @@
+const { FaleConosco } = require('../database/models')
+
 const FaleConoscoController = {
     viewForm: (req, res) => {
         return res.render('fale_conosco', { title: 'Fale Conosco', linkHome:'/', linkLogin: '/', loginCadastroUsuario: 'Cadastro'})
     },
-    envioForm: (req, res) => {
+    envioForm: async (req, res) => {
+        let {nome, email, mensagem} = req.body
+
+        await FaleConosco.create({
+            nome,
+            data: new Date(),
+            mensagem,
+            email
+        })
+
         res.locals.mensagemEnviada = true
         return res.render('fale_conosco', { title: 'Fale Conosco', linkHome:'/', linkLogin: '/', loginCadastroUsuario: 'Cadastro'})
     }
